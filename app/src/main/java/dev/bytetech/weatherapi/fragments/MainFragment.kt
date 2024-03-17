beb170b0e241b4b062ca47a430c713cfed5f5494
Pi_p <pg0938382@gmail.com>
2024-03-17 07:04:46 +0700

    update code

--- app/src/main/java/dev/bytetech/weatherapi/fragments/MainFragment.kt
+++ app/src/main/java/dev/bytetech/weatherapi/fragments/MainFragment.kt
@@ -1,7 +1,12 @@
 package dev.bytetech.weatherapi.fragments
 
 import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,13 +15,20 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.activityViewModels
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationTokenSource
 import com.google.android.material.tabs.TabLayoutMediator
 import com.squareup.picasso.Picasso
+import dev.bytetech.weatherapi.DialogManager
 import dev.bytetech.weatherapi.MainViewModel
 import dev.bytetech.weatherapi.adapters.VpAdapter
 import dev.bytetech.weatherapi.adapters.WeatherAdapter
@@ -27,6 +39,7 @@ import org.json.JSONObject
 const val API_KEY = "" // enter your API key from this site "https://www.weatherapi.com/"
 
 class MainFragment : Fragment() {
+    private lateinit var fLocationClient: FusedLocationProviderClient
     private val fList = listOf(
         hours.newInstance(),
         days.newInstance()
@@ -51,16 +64,67 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         chekPermission()
         init()
-        requestWeaterData("Empty")
         updateCurrentCard()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkLocation()
     }
 
     private fun init() = with(binds) {
+        fLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
         val adapter = VpAdapter(activity as FragmentActivity, fList)
         viewPager.adapter = adapter
         TabLayoutMediator(tabLayout, viewPager) { tab, pos ->
             tab.text = tList[pos]
         }.attach()
+        ibReserch.setOnClickListener {
+            tabLayout.selectTab(tabLayout.getTabAt(0))
+            checkLocation()
+        }
+        ibSerch.setOnClickListener {
+            DialogManager.serchByNameDialog(requireContext(), object : DialogManager.Listener{
+                override fun onClick(name: String?) {
+                    name?.let { it1 -> requestWeaterData(it1) }
+                }
+            })
+        }
+    }
+
+    private fun checkLocation(){
+        if (isLocationEnabled()){
+            getLocation()
+        } else {
+            DialogManager.locationSettingsDialog(requireContext(), object : DialogManager.Listener{
+                override fun onClick(name: String?) {
+                    startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+                }
+            })
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean{
+        val lm = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return lm.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    }
+
+    private fun getLocation(){
+        val ct = CancellationTokenSource()
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, ct.token).addOnCompleteListener {
+            requestWeaterData("${it.result.latitude},${it.result.longitude} ")
+        }
     }
 
     private fun updateCurrentCard() = with(binds){
